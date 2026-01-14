cc37012d36c10d20f743feae16ba84f1b5f113dd
vignesh33654 <ssvignesh.619@gmail.com>
2026-01-14 11:21:02 +0530

    updated the local trainNumber

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   baseURL: 'https://api.railradar.in/api/v1',
   apiKey: 'rr_9mo485czvt0q3om47ck4cylpavtcvi2w',
-  trainNumber: '12618',
+  trainNumber: '43763',
   timeout: 15000,
 };
